655a7bbdab8de5ce34ea0fca41bcdc1373585801
Gregory Demin <gdemin@gmail.com>
2017-07-23 02:34:30 +0300

    Initial commit

---
+++ R/antitibble.R
@@ -0,0 +1,23 @@
+
+#' @export
+print.tbl_df = print.data.frame
+
+
+#' @export
+'[.tbl_df' = `[.data.frame`
+
+#' @export
+'[<-.tbl_df' = `[<-.data.frame`
+
+
+#' @export
+'[[.tbl_df' = `[[.data.frame`
+
+#' @export
+'[[<-.tbl_df' = `[[<-.data.frame`
+
+#' @export
+'$.tbl_df' = `$.data.frame`
+
+#' @export
+'$<-.tbl_df' = `$<-.data.frame`
